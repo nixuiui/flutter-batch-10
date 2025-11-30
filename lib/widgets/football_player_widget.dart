4dbbbef089b01248256d849ce7bc90121cedc4a3
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2025-11-30 20:40:33 +0700

    Enhance Football Player Page and Widget with favorite functionality and player images

--- lib/widgets/football_player_widget.dart
+++ lib/widgets/football_player_widget.dart
@@ -5,49 +5,63 @@ class FootballPlayerWidget extends StatelessWidget {
   final String name;
   final String country;
   final String? imageUrl;
+  final bool isFavorite;
+  final Function()? onTap;
 
   const FootballPlayerWidget({
     super.key,
     required this.name,
     required this.country,
     this.imageUrl,
+    this.isFavorite = false,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          spacing: 16,
-          children: [
-            CircleAvatar(
-              foregroundImage: imageUrl != null ? NetworkImage(imageUrl!) : null,
-              child: Text(name.isNotEmpty ? name[0] : "?"),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 14, 
-                    fontWeight: FontWeight.bold,
-                  )
+    return GestureDetector(
+      onTap: onTap?.call,
+      child: Card(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            spacing: 16,
+            children: [
+              CircleAvatar(
+                foregroundImage: imageUrl != null ? NetworkImage(imageUrl!) : null,
+                child: Text(name.isNotEmpty ? name[0] : "?"),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 14, 
+                        fontWeight: FontWeight.bold,
+                      )
+                    ),
+                    Text(
+                      country,
+                      style: TextStyle(
+                        fontSize: 12,
+                      )
+                    ),
+                  ],
                 ),
-                Text(
-                  country,
-                  style: TextStyle(
-                    fontSize: 12,
-                  )
+              ),
+              if (isFavorite) 
+                Icon(
+                  Icons.star,
+                  color: Colors.amber,
                 ),
-              ],
-            ),
-          ],
+            ],
+          )
         )
-      )
+      ),
     );
   }
 }
